953e934b8e20b3435b13734c422351f3df54d692
Aleks Clark <aleks.clark@gmail.com>
2020-04-09 15:37:49 -0500

    dockerify development

---
+++ docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+gem install bundler:2.1.4
+
+# Ensure all gems installed.
+bundle check || bundle install
+
+bundle binstubs bundler --force
+
+# Add binstubs to bin which has been added to PATH in Dockerfile.
+bundle exec spring binstub --all
+
+bin/yarn install --check-files
+
+bin/rake db:exists && bin/rake db:migrate || bin/rake db:setup
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
