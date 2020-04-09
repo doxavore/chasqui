0e276b318c8e9d9632169120e254ff6e8715c1ba
Doug Mayer <doug.mayer@joinroot.com>
2020-04-09 18:59:49 -0500

    Perform checks against postgres and redis for deployment check.

--- app/controllers/dokku_controller.rb
+++ app/controllers/dokku_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class DokkuController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def deploy_check
+    ActiveRecord::Base.connection.active?
+
+    Resque.redis.server_time
+
     render plain: "deploy_ok"
   end
 end
