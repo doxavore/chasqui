b15e246337bafced11565f4c2205ecc95578dbfc
Doug Mayer <doug.mayer@joinroot.com>
2020-04-09 12:41:58 -0500

    Configure locales.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#show"
+
   get "dokku/deploy_check", to: "dokku#deploy_check"
 end
