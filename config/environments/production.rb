9298ab2b378d185deb140c3f7288d6a921be84af
Doug Mayer <me@doxavore.com>
2020-04-09 17:48:08 -0500

    Add resque and use it from ActiveJob.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  # Use a real queuing backend for Active Job.
+  config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "chasqui_production"
 
   config.action_mailer.perform_caching = false
