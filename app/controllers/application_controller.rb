a4dbbcdbbe680c06558543bc2d90c7f322fca093
Doug Mayer <doug.mayer@joinroot.com>
2020-04-09 13:08:51 -0500

    Persist explicit locale selection in a cookie.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def detect_locale
+    cookies[:locale] ||
+      request.env["HTTP_ACCEPT_LANGUAGE"].to_s.scan(/^[a-z]{2}/).first ||
+      I18n.default_locale
+  end
+
   def switch_locale(&action)
-    locale = params[:locale] || request.env["HTTP_ACCEPT_LANGUAGE"].to_s.scan(/^[a-z]{2}/).first || I18n.default_locale
+    cookies[:locale] = params[:locale] if params[:locale]
+    locale = detect_locale
     I18n.with_locale(locale, &action)
   end
 end
