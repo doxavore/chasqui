721f189296aa923e25ddc26f29dea641dc169707
Doug Mayer <doug.mayer@joinroot.com>
2020-04-09 19:04:13 -0500

    Add BUILDPACK_URL to avoid Dokku picking up Dockerfile.

--- app/controllers/dokku_controller.rb
+++ app/controllers/dokku_controller.rb
@@ -4,9 +4,9 @@ class DokkuController < ApplicationController
   skip_before_action :authenticate_user!
 
   def deploy_check
-    ActiveRecord::Base.connection.active?
+    ActiveRecord::Base.connection.active? || raise("Postgres connection failed")
 
-    Resque.redis.server_time
+    Resque.redis.server_time || raise("Redis connection failed")
 
     render plain: "deploy_ok"
   end
