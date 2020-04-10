af795a3096409002ec4849a412952d8bea2fb4b8
Doug Mayer <doug.mayer@joinroot.com>
2020-04-10 08:38:52 -0500

    docker/exec for running container, allow running tests outside of docker.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,9 @@
-Resque.redis = ENV.fetch("REDIS_URL")
+redis_url = ENV.fetch("REDIS_URL") do |key|
+  if Rails.env.development? || Rails.env.test?
+    "redis://localhost:6379"
+  else
+    raise KeyError, "Key not found: \"#{key}\""
+  end
+end
+
+Resque.redis = redis_url
