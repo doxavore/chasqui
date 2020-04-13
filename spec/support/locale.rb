6022d4e07b5f401a0f500e76f83913c2e6c2518b
Doug Mayer <me@doxavore.com>
2020-04-13 10:53:26 -0500

    Add set_locale and with_locale spec helpers.

---
+++ spec/support/locale.rb
@@ -0,0 +1,19 @@
+require "i18n"
+
+module LocaleSpecHelper
+  def self.included(base)
+    base.extend(ExampleGroupMethods)
+  end
+
+  def with_locale(new_locale, &block)
+    I18n.with_locale(new_locale, &block)
+  end
+
+  module ExampleGroupMethods
+    def set_locale(new_locale)
+      around do |example|
+        with_locale(new_locale) { example.run }
+      end
+    end
+  end
+end
