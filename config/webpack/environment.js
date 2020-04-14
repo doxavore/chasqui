2754cc172f420c52ee5bb685a6ee0e1640001652
Doug Mayer <me@doxavore.com>
2020-04-13 19:47:19 -0500

    Split application and login javascripts, configure webpack.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
+
+environment.splitChunks();
+
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"]
+  })
+);
 
 module.exports = environment;
