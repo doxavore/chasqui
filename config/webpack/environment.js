70a060563fc340703d265d41da5ba23b26e006f3
Doug Mayer <me@doxavore.com>
2020-04-18 21:38:41 -0500

    Fix asset building, etc.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,43 @@
 const { environment } = require("@rails/webpacker");
 const webpack = require("webpack");
+const miniSvgDataUri = require('mini-svg-data-uri');
+
+const SVG_FILE_TEST = /\.svg$/i;
+const URL_LOADER_TEST = /\.(?:svg|png|gif|jpe?g)$/i;
+
+environment.loaders.prepend(
+  "url",
+  {
+    test: URL_LOADER_TEST,
+    use: [{
+      loader: "url-loader",
+      options: {
+        limit: 4096,
+        name: "[name]-[hash].[ext]",
+        generator: (content, mimeType, encoding, resourcePath) => {
+          if (SVG_FILE_TEST.test(resourcePath)) {
+            return miniSvgDataUri(content.toString());
+          }
+
+          // Fallback to the default.
+          // See https://github.com/webpack-contrib/url-loader/.
+          return `data:${mimetype}${encoding ? `;${encoding}` : ''},${content.toString(
+            encoding || undefined
+          )}`;
+        },
+      },
+    }],
+  },
+);
+
+// Avoid using both file and url loaders together.
+environment.loaders.get('file').exclude = URL_LOADER_TEST;
+
+// resolve-url-loader must be used before sass-loader.
+// See https://github.com/rails/webpacker/blob/master/docs/css.md#resolve-url-loader.
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: "resolve-url-loader",
+});
 
 environment.splitChunks();
 
@@ -7,8 +45,8 @@ environment.plugins.prepend(
   "Provide",
   new webpack.ProvidePlugin({
     jQuery: "jquery",
-    Popper: ["popper.js", "default"]
-  })
+    Popper: ["popper.js", "default"],
+  }),
 );
 
 module.exports = environment;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+admin_address = Address.create!(
+    line_1: "Pasaje Moquegua, 203",
+    line_2: "Urb. San Felipe",
+    locality: "Comas",
+    administrative_area: "Lima",
+    country: "PE"
+)
+
+admin_user = User.create!(
+  email: "admin@example.com",
+  password: "admintest",
+  password_confirmation: "admintest",
+  address: admin_address,
+  admin: true
+)
