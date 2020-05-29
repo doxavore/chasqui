88b9c4372f349ed572485ac5d2aadee111bb5467
Doug Mayer <me@doxavore.com>
2020-05-29 16:55:26 -0500

    Expose Resque web to admins.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require "resque/server"
+
 Rails.application.routes.draw do
   devise_for :users
 
   ActiveAdmin.routes(self)
 
   root to: "admin/dashboard#index"
+
+  authenticate :user, lambda { |user| user.admin? } do
+    mount Resque::Server, at: "/admin/resque"
+  end
 
   get "dokku/deploy_check", to: "dokku#deploy_check"
 end
