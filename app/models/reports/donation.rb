bfd049ef1d81ea2ad43bc21d3e82d155e2d890f8
Aleks Clark <aleks.clark@gmail.com>
2020-06-13 06:56:35 -0500

    wip

--- app/models/reports/donation.rb
+++ app/models/reports/donation.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module Reports
-  class Donation
+  class Donation < Base
+    def receipts
+      @recipts ||= Receipt.completed
+                          .where(delivered_at: start_date..end_date)
+                          .where(destination_type: 'ExternalEntity')
+    end
+
+    def products
+    end
+  end
+end
