0e2c99fd809686ab12d23993a2379b61f6210fa9
Aleks Clark <aleks.clark@gmail.com>
2020-06-30 06:21:13 -0500

    set timezone to Lima

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Chasqui
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.active_storage.variant_processor = :vips
+    config.time_zone = 'Lima'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
