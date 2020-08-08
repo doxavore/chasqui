cea38e828c3866a85844ecea61a4b8459b71deda
Aleks Clark <aleks.clark@gmail.com>
2020-08-08 17:52:12 -0500

    IGNORE NOTHING

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
   config.dsn = ENV.fetch("SENTRY_DSN", '')
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.excluded_exceptions = []
 end
